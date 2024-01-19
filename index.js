f94c524d9d7c37edce8d0e261a804903f364b630
Bhavik Gevariya <bgevariya20@gmail.com>
2024-01-19 10:06:26 -0500

    updated netlyfy url

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import {connectToDB} from "./utils.js";
+import { connectToDB } from "./utils.js";
 import indexRouter from "./routes/index.js";
 import bodyParser from "body-parser";
 import session from "express-session";
 import cookieParser from "cookie-parser";
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 dotenv.config();
 
@@ -17,27 +17,33 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 // app.use(cors());
 const corsOptions = {
-    origin: ["http://localhost:3000", "http://localhost:5173"], // Replace with your frontend's URL
-    optionsSuccessStatus: 200,
-    credentials: true
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://test-nursing-sem4.netlify.app",
+  ], // Replace with your frontend's URL
+  optionsSuccessStatus: 200,
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.use(cookieParser());
- 
-app.use(session({
+
+app.use(
+  session({
     secret: "drthrthvfr",
     saveUninitialized: true,
-    resave: true
-}));
+    resave: true,
+  })
+);
 
 app.use("", indexRouter);
 
 (async function init() {
-    try {
-        await connectToDB();
-        app.listen(port, () => console.log(`Server is listening on port ${port}`));
-    } catch (err) {
-        console.warn(err);
-    }
-}());
+  try {
+    await connectToDB();
+    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+  } catch (err) {
+    console.warn(err);
+  }
+})();
