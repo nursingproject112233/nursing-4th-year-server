53516f149050ff5129c41e153f34a89c0acd15d8
Bhavik Gevariya <bgevariya20@gmail.com>
2024-01-23 10:29:42 -0500

    flag changed

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(
     secret: "drthrthvfr",
     saveUninitialized: true,
     resave: true,
-    cookie: { secure: false, sameSite: "none" },
+    cookie: { secure: false, sameSite: "lax" },
   })
 );
 
