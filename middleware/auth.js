efd626808a6e7af32c92b9baf9024faf68fff8aa
Bhavik Gevariya <bgevariya20@gmail.com>
2024-01-24 09:51:00 -0500

    changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,12 +3,12 @@ const auth = (req, res, next) => {
     if (!req.session.user) {
       return res.status(400).json({ message: "Login required" });
     }
-    let jsonParse = JSON.parse(req.session.user);
+    // let jsonParse = JSON.parse(req.session.user);
 
-    req.session.user = jsonParse;
-    console.log("====================================");
-    console.log(req.session);
-    console.log("====================================");
+    // req.session.user = jsonParse;
+    // console.log("====================================");
+    // console.log(req.session);
+    // console.log("====================================");
     next();
   } catch (error) {
     return res.status(500).json({ error: error.message });
