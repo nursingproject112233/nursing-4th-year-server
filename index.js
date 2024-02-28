c46d8f16d1aeec6b0311799c8e9d42ad51477581
Bhavik Gevariya <bgevariya20@gmail.com>
2024-02-28 08:17:42 -0500

    update

--- index.js
+++ index.js
@@ -11,27 +11,28 @@ import { default as connectMongoDBSession } from "connect-mongodb-session";
 import indexRouter from "./routes/index.js";
 import { connectToDB } from "./utils.js";
 
-mongoose.set("strictQuery", false);
-const MongoDBStore = connectMongoDBSession(session);
+// mongoose.set("strictQuery", false);
+// const MongoDBStore = connectMongoDBSession(session);
 dotenv.config();
-const mongodbURI = "mongodb+srv://user:user@cluster0.kctlxex.mongodb.net/?retryWrites=true&w=majority";
+const mongodbURI =
+  "mongodb+srv://user:user@cluster0.kctlxex.mongodb.net/?retryWrites=true&w=majority";
 const store = new MongoDBStore({
-    // uri: "mongodb+srv://user:user@cluster0.kctlxex.mongodb.net/?retryWrites=true&w=majority",
-    uri: process.env.mongodbURI,
-    collection: "mySessions"
+  // uri: "mongodb+srv://user:user@cluster0.kctlxex.mongodb.net/?retryWrites=true&w=majority",
+  uri: process.env.mongodbURI,
+  collection: "mySessions",
 });
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 // app.use(cors());
 const corsOptions = {
-    origin: [
-        "http://localhost:8000",
-        "http://localhost:3000",
-        "https://test-nursing-sem4.netlify.app"
-    ], // Replace with your frontend's URL
-    optionsSuccessStatus: 200,
-    credentials: true
+  origin: [
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "https://test-nursing-sem4.netlify.app",
+  ], // Replace with your frontend's URL
+  optionsSuccessStatus: 200,
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -39,33 +40,33 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.set("trust proxy", 1);
 app.use(
-    session({
-        proxy: true,
-        name: "universityNursing",
-        secret: "drthrthvfr",
-        saveUninitialized: true,
-        resave: true,
+  session({
+    proxy: true,
+    name: "universityNursing",
+    secret: "drthrthvfr",
+    saveUninitialized: true,
+    resave: true,
 
-        cookie: {
-            httpOnly: true,
-            secure: true,
-            maxAge: 1000 * 60 * 60 * 48,
-            sameSite: "none"
-        }
-        // store: store,
-    })
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 48,
+      sameSite: "none",
+    },
+    // store: store,
+  })
 );
 
 app.use("/", indexRouter);
 
 (async function init() {
-    try {
-        await connectToDB();
-        app.listen(port, () => console.log(`Server is listening on port ${port}`));
-    } catch (err) {
-        console.warn(err);
-    }
-}());
+  try {
+    await connectToDB();
+    app.listen(port, () => console.log(`Server is listening on port ${port}`));
+  } catch (err) {
+    console.warn(err);
+  }
+})();
 
 /* eslint-disable spaced-comment */
 /* eslint-disable no-multiple-empty-lines */
